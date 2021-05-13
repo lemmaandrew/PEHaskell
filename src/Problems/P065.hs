a90bf838f800f9af4bdcc218483f28a649e66b43
peterjayandrew <peterjayandrew@gmail.com>
2021-05-13 17:42:24 -0500

    Did P065

--- src/Problems/P065.hs
+++ src/Problems/P065.hs
@@ -2,5 +2,18 @@ module Problems.P065
     ( p065
     ) where
 
+import Data.List ( inits )
+import Data.Ratio ( numerator )
+import EulerMath ( sumDigits )
+
+eTerms :: (Enum a, Num a) => [a]
+eTerms = 2 : 1 : 2 : ([4,6 ..] >>= \x -> [1, 1, x])
+
+contFrac :: (Foldable t, Fractional a) => t a -> a
+contFrac = foldr1 (\a b -> a + 1 / b)
+
+eConts :: [Rational]
+eConts = map contFrac $ tail $ inits eTerms
+
 p065 :: IO ()
-p065 = putStrLn "Lol didn't do it yet lmao"
+p065 = print $ sumDigits (numerator $ eConts !! 99)
